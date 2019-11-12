4305c2b15bb887d84280b7c569f4f104d4a6b278
amaljoy <amal.joy.tk@gmail.com>
2019-11-12 15:51:17 +0530

    Initial commit

---
+++ app/src/main/java/com/example/progressbar/MainActivity.java
@@ -0,0 +1,48 @@
+package com.example.progressbar;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.os.Handler;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private ProgressBar progressBar;
+    private int progressStatus = 0;
+    private TextView textView;
+    private Handler handler = new Handler();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+       progressBar = findViewById(R.id.progress_circular);
+       textView = findViewById(R.id.textview);
+
+       new Thread(new Runnable() {
+           @Override
+           public void run() {
+               while (progressStatus < 100){
+                   progressStatus += 1;
+                   handler.post(new Runnable() {
+                       @Override
+                       public void run() {
+                           progressBar.setProgress(progressStatus);
+                           textView.setText(progressStatus+"/"+progressBar.getMax());
+                       }
+                   });
+                   try {
+                       Thread.sleep(200);
+                   }
+                   catch (InterruptedException e){
+                       e.printStackTrace();
+                   }
+               }
+           }
+       }).start();
+
+    }
+}
